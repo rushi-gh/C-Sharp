cc6897a074d9fd6dc124f0dd0f895f278ee2af17
Rushikesh Naikwadi <rnaikwadi.mailme@gmail.com>
2024-06-26 00:15:56 +0530

    Arrays in C#

---
+++ To Do List/Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = new int[5];
+
+        int[] nums = new int[] { 1, 2, 3, 4, 5, 6 };
+        int sum = 0;
+        for (int i = 0; i < nums.GetLength(0); i++)
+        {
+            sum += nums[i];
+        }
+        Console.WriteLine(sum);
+
+        int[,] twoDim = new int[2, 3]
+        {
+            {1, 2, 3 },
+            {4, 5, 6 }
+        };
+    }
+}
