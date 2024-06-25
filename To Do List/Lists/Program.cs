4498e7273557288223c435b79b53683835dd69e2
Rushikesh Naikwadi <rnaikwadi.mailme@gmail.com>
2024-06-26 01:19:16 +0530

    List Collection in C#

---
+++ To Do List/Lists/Program.cs
@@ -0,0 +1,62 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List <int> ints = new List <int> ();
+        Console.WriteLine($"Size of ints list : {ints.Count}");
+
+        ints.Add(1);
+        Console.WriteLine($"Size of ints list : {ints.Count}");
+
+
+        Console.WriteLine("-----------------------------------");
+
+
+        var days = new List<string>()
+        {
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        };
+
+        foreach (var item in days)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+
+
+        days.Remove("Monday");
+        foreach (var item in days)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+
+        days.RemoveAt(days.Count - 1);
+        foreach (var item in days)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+
+        days.AddRange(new[] { "Jan", "Feb", "Mar" });
+        foreach (var item in days)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Index of Thursday : {days.IndexOf("Thursday")}");
+        Console.WriteLine($"Index of Monday : {days.IndexOf("Monday")}");
+
+        Console.WriteLine("Contains Monday " + days.Contains("Monday"));
+        Console.WriteLine("Contains Thursday " + days.Contains("Thursday"));
+
+
+        days.Clear();
+    }
+}
