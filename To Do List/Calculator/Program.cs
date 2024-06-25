fedb6c8d6a2e768089f7ecc85d21fbd28bce3578
Rushikesh Naikwadi <rnaikwadi.mailme@gmail.com>
2024-06-25 23:19:52 +0530

    Basic Calculator App

--- To Do List/Calculator/Program.cs
+++ To Do List/Calculator/Program.cs
@@ -2,6 +2,53 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Starting Calculator!");
+        bool @continue = true;
+        while (@continue)
+        {
+            Console.Write("Enter first number : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int ans = 0;
+            Console.WriteLine("For Addition Press 1");
+            Console.WriteLine("For Substraction Press 2");
+            Console.WriteLine("For Multiplication Press 3");
+            Console.WriteLine("For Division Press 4");
+            Console.WriteLine();
+            
+            int opr = int.Parse(Console.ReadLine());
+            switch (opr)
+            {
+                case 1:
+                    ans = num1 + num2;
+                    break;
+                case 2:
+                    ans = num1 - num2;
+                    break;
+                case 3:
+                    ans = num1 * num2;
+                    break;
+                case 4:
+                    ans = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine($"Ans = {ans}");
+            Console.WriteLine();
+
+            Console.WriteLine("Press 1 for another opration");
+            Console.WriteLine("Else press 0");
+            Console.WriteLine();
+
+            int inp = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (inp == 0)
+            {
+                @continue = false;
+            }
+            Console.Clear();
+        }
     }
 }
