2ee4966194e4efe3904f7cf70e6ef93be5cd6c02
Rushikesh Naikwadi <rnaikwadi.mailme@gmail.com>
2024-06-29 15:59:06 +0530

    Function and Constructor Overloading

---
+++ OOP/Overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿// Function Overloading
+class Student
+{
+    private string _firstName;
+    private string _lastName;
+
+    public void SetName(string firstName)
+    {
+        _firstName = firstName;
+    }
+
+    public void SetName(string firstName, string lastName)
+    {
+        SetName(firstName);
+        _lastName = lastName;
+    }
+}
+
+namespace Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student rushi = new Student();
+            rushi.SetName("Rushi", "Naikwadi");
+        }
+    }
+}
+
