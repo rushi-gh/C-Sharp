b9806a7b4d9135f0033f72c9311aa2898275c7ef
Rushikesh Naikwadi <rnaikwadi.mailme@gmail.com>
2024-06-29 14:50:51 +0530

    Implement basic Rectangle Class

---
+++ OOP/Rectangle_Class/Program.cs
@@ -0,0 +1,49 @@
+﻿class Rectangle
+{
+    public int Width;
+    public int Height;
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public void PrintDimensions()
+    {
+        Console.WriteLine($"Width of Rectangle : {Width}");
+        Console.WriteLine($"Height of Rectangle : {Height}");
+        Console.WriteLine("--------------------------------");
+    }
+
+    public int GetCircumference()
+    {
+        return 2 * (Height + Width);
+    }
+
+    public int GetArea()
+    {
+        return (Width * Height);
+    }
+
+    public void Print()
+    {
+        PrintDimensions();
+        Console.WriteLine($"Area of Rectangle : {GetArea()}");
+        Console.WriteLine($"Circumference of Rectangle : {GetCircumference()}");
+        Console.WriteLine("================================");
+    }
+}
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Rectangle one = new Rectangle(2, 9);
+        Rectangle two = new Rectangle(3, 5);
+
+        one.Print();
+        two.Print(); 
+    }
+}
