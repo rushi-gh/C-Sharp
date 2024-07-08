9bd711fb4471140c3109e7523cd67318f46cc803
Rushikesh Naikwadi <rnaikwadi.mailme@gmail.com>
2024-07-09 01:05:03 +0530

    Static Class, Methods, Data, ...

---
+++ OOP/Static/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Static {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Calculator.Add(2, 4));
+        }
+    }
+}
+
+static class Calculator {
+    public static string name;
+
+    public static int Add(int x, int y) => x + y;
+
+    public static int Subtract (int  x, int y) => x - y;
+
+    public static int Multiply(int x, int y) => x * y;
+
+    public static int Divide(int x, int y) => x / y;
+}
